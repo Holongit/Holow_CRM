a6a57b4ee5295181acfaedfc7ae7a3c9cc2dc5c5
Holongit <AlarmX42@gmail.com>
2024-01-05 09:18:47 +0100

    status, Rodzaj Naprawy, colory v 0.0.5

--- base/settings.py
+++ base/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v2*%%-^^vmx@gp21xn2g(5-l2udj==j4424$lza-m7xdfaa^jk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.31.33', 'localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['192.168.31.33', 'localhost', '127.0.0.1', '0.0.0.0', '192.168.1.39', '192.168.1.41']
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
