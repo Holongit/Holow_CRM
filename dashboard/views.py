5b7ed6bdee62c6f3a31ad4fc64f57fb54846cff9
Holongit <AlarmX42@gmail.com>
2024-01-13 21:01:17 +0100

    login, view, pracowniki v 0.0.7

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from gadgets.models import *
+from notes.form import *
 
 
 @login_required(login_url='login')
 def index_dash(request):
+    notatki = Note.objects.all()
     gadgets_all = Gadget.objects.all()
     qnt_gadgetsq = gadgets_all.__len__()
     gadgets_ok = gadgets_all.filter(status__icontains='GOTOWY').__len__()
@@ -16,7 +18,8 @@ def index_dash(request):
         'gadgets_serwis': gadgets_serwis,
         'gadgets_ok': gadgets_ok,
         'qnt_gadgets': qnt_gadgetsq,
-        'gadgets_czeka': gadgets_czeka
+        'gadgets_czeka': gadgets_czeka,
+        'notatki': notatki,
     }
 
     return render(request, 'dashboard.html', context=context)
